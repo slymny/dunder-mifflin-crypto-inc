d6c28c99c0d9633dc4a764a1554cccd3a748ae88
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-02 15:04:32 +0200

    Constants file created and the constant variables are defined

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+ The constants file is used to store anything 
+ that multiple files use, that should ALWAYS be the same
+ 
+ It is an industry standard to make these variables fully capitalised
+*/
+
+export const USER_INTERFACE_ID = 'user-interface';
+export const GLOBAL_INFO_ID = 'global-info';
+export const COINS_ROW_ID = 'coins';
+export const API_BASE_URL = 'https://api.coinpaprika.com/v1/';
+export const GLOBAL_INFO = 'global';
+export const TICKERS = 'tickers/';
+
+export const ANSWERS_LIST_ID = 'answers-list';
+export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
+
+export const ARRAY_OF_COIN_IDS = ["btc-bitcoin", "eth-ethereum", "usdt-tether" , "bnb-binance-coin", "usdc-usd-coin", "hex-hex", "xrp-xrp", "ada-cardano", "luna-terra"/*, "sol-solana", "avax-avalanche", "dot-polkadot", "doge-dogecoin", "busd-binance-usd", "ust-terrausd", "matic-polygon", "shib-shiba-inu", "near-near-protocol", "steth-lido-staked-ether" */];
+//export const ARRAY_OF_COIN_IDS = ["btc-bitcoin", "eth-ethereum", "usdt-tether" , "bnb-binance-coin", "usdc-usd-coin", "hex-hex", "xrp-xrp", "ada-cardano", "luna-terra"/*, "sol-solana", "avax-avalanche", "dot-polkadot", "doge-dogecoin", "busd-binance-usd", "ust-terrausd", "matic-polygon", "shib-shiba-inu", "near-near-protocol", "steth-lido-staked-ether" */];
+
