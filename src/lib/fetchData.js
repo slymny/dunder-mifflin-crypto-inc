cfe1e2f0d53e54271b7d2c622d64bbb9795d9364
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-02 15:01:45 +0200

    Fetch logic is created

---
+++ src/lib/fetchData.js
@@ -0,0 +1,25 @@
+
+const HTTP_STATUS_NO_CONTENT = 204;
+
+/**
+ * Fetch data using an HTTP GET request and optionally cache the response.
+ * @param {string} url The url to fetch from.
+ */
+export async function fetchData(url) {
+  let data;
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`HTTP ${res.status}  ${res.statusText}`);
+  }
+
+  if (res.status === HTTP_STATUS_NO_CONTENT) {
+    data = null;
+  } else {
+    data = await res.json();
+  }
+
+  return data;
+}
+
+//export default fetchData;
