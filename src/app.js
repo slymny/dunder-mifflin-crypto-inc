43e6ea66c878226b34fb77dafcac57b2e561a127
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-03 14:56:44 +0200

    Navbar price update  is added

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@
 
 
 import { initLandingPage } from './pages/landingPage.js';
-import { COINS_ROW_ID, GLOBAL_INFO_ID } from './constants.js';
+import { COINS_ROW_ID, GLOBAL_INFO_ID, NAVBAR_COINS_ID } from './constants.js';
 
 const loadApp = () => {
   initLandingPage();
   setInterval(() => {
     document.getElementById(GLOBAL_INFO_ID).innerHTML = '';
     document.getElementById(COINS_ROW_ID).innerHTML = '';
+    document.getElementById(NAVBAR_COINS_ID).innerHTML = '';
+
     initLandingPage()
   }, 30000);
   
