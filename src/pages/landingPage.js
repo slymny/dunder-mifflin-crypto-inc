c42a6e6bc12fb232ab8729bb7dd3129d6c29db34
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-04 11:17:11 +0200

    landing page is created

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -2,7 +2,6 @@
 
 import { USER_INTERFACE_ID, GLOBAL_INFO_ID, API_BASE_URL, GLOBAL_INFO, ARRAY_OF_COIN_IDS, TICKERS, COINS } from '../constants.js';
 import { createGlobalInfo, createCoinsTable } from '../views/landingView.js';
-import { initChartPage } from './questionPage.js';
 import { fetchData } from '../lib/fetchData.js';
 import { moneyFormatter } from '../lib/moneyFormatter.js';
 
@@ -25,33 +24,45 @@ export const initLandingPage = async () => {
     const coinRow = createCoinsTable(coin)
     table.appendChild(coinRow);
 
-    coinRow.addEventListener('click', async (e) => {
-      const response = await fetchData(`${API_BASE_URL}${COINS}${coin.id}`);
-      window.open(response.links.explorer[0]);
-    });
-    coinRow.style.cursor = 'pointer';
+    coinRow.onclick = function(e) {
+      openExplorerPage(coin);
+    };
   });
 
   const navbarCoins = coinsInfo.slice(0, 5);
   const navbarCoinsDiv = document.getElementById('navbar-coins');
   navbarCoins.forEach(coin => {
-    const coinDiv = createCoinDiv(coin);
+    const coinDiv = createNavbarCoinDiv(coin);
+    coinDiv.onclick = function() {
+      openExplorerPage(coin);
+    }
     navbarCoinsDiv.appendChild(coinDiv);
   })
 
 };
 
-function createCoinDiv(coin) {
+async function openExplorerPage(coin) {
+  const response = await fetchData(`${API_BASE_URL}${COINS}${coin.id}`);
+  window.open(response.links.explorer[0]);
+};
+// alternative: goes to static web page with the id of the coin
+
+function createNavbarCoinDiv(coin) {
   const coinDiv = document.createElement('div');
-    coinDiv.className = 'navbar-coin';
-    const coinSymbol = document.createElement('h3');
-    const coinPrice = document.createElement('h3');
-    coinSymbol.textContent = coin.symbol;
-    coinPrice.textContent = moneyFormatter.format(coin.quotes.USD.price);
-    coinDiv.append(coinSymbol, coinPrice);
+  coinDiv.className = 'navbar-coin';
+  const coinSymbol = document.createElement('h3');
+  const coinPrice = document.createElement('h3');
+  coinSymbol.textContent = coin.symbol;
+  const price = coin.quotes.USD.price;
+  const change = coin.quotes.USD.percent_change_15m;
+  coinPrice.textContent = moneyFormatter.format(price);
+  coinDiv.append(coinSymbol, coinPrice);
+  if(change < 0) {
+    coinDiv.classList.add('decreasing');
+  } else if(change > 0) {
+    coinDiv.classList.add('increasing');
+  };
     return coinDiv;
 }
 
-const startQuiz = () => {
-  initChartPage();
-};
+
