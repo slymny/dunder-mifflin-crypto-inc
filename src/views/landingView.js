6726cc18c6830560d97a784d6efe73020f326f4e
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-04 11:17:43 +0200

    landing page view is created

--- src/views/landingView.js
+++ src/views/landingView.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//import { START_QUIZ_BUTTON_ID } from '../constants.js';
+import {moneyFormatter} from '../lib/moneyFormatter.js'
 
 /**
  * Create the welcome screen
@@ -8,41 +8,68 @@
  */
 export const createGlobalInfo = (props) => {
   const element = document.createElement('div');
+  element.className = 'global-info';
   element.innerHTML = String.raw`
     <h1>Global CryptoCurrency Market Info</h1>
-    <p><span class="glb-value-name">Market Cap:</span>${props.market_cap_usd} 
-    <span class="glb-value-name">Volume 24h:</span>${props.volume_24h_change_24h}
-    <span class="glb-value-name">Market Cap:</span>${props.market_cap_change_24h}
-    <span class="glb-value-name">Market Cap ATH Value:</span>${props.market_cap_ath_value}
-    <span class="glb-value-name">Bitcoin Dominance:</span>${props.bitcoin_dominance_percentage}
-    <span class="glb-value-name">Last Update at:</span>${props.last_updated}</p>`
+    <br>
+    
+    <p>
+    <span class="glb-value-name">Market Cap: </span>${moneyFormatter.format(props.market_cap_usd)}    
+    <span class="glb-value-name">Volume 24h: </span><span class="global-change">${props.volume_24h_change_24h}</span>
+    <span class="glb-value-name">Market Cap: </span><span class="global-change">${props.market_cap_change_24h}</span>  
+    <span class="glb-value-name">Market Cap ATH Value: </span>${moneyFormatter.format(props.market_cap_ath_value)}  
+    <span class="glb-value-name">Bitcoin Dominance: </span>${props.bitcoin_dominance_percentage}</p>
+    `
+    document.querySelectorAll('.global-change').forEach(elem => {
+      console.log(elem);
+      // if(Number(elem.innerText) < 0) {
+      //   elem.classList.add('decreasing');
+      // } else if(Number(elem.innerText) > 0) {
+      //   elem.classList.add('increasing');
+      // };
+    });
   return element;
 };
 
 
 export const createCoinsTable = (props) => {
   const element = document.createElement('tr');
-  const {name, symbol, rank, quotes:{USD:{price, market_cap, volume_24h, percent_change_30m, percent_change_1h, percent_change_6h, percent_change_12h, percent_change_24h, percent_change_7d, percent_change_30d, percent_change_1y, ath_price, percent_from_price_ath}}} = props;
-  element.innerHTML = String.raw`
-      <td>${name}</td>
-      <td>${symbol}</td>
-      <td>${rank}</td>
-      <td>${price}</td>
-      <td>${market_cap}</td>
-      <td>${volume_24h}</td>
-      <td>${percent_change_30m}</td>
-      <td>${percent_change_1h}</td>
-      <td>${percent_change_6h}</td>
-      <td>${percent_change_12h}</td>
-      <td>${percent_change_24h}</td>
-      <td>${percent_change_7d}</td>
-      <td>${percent_change_30d}</td>
-      <td>${percent_change_1y}</td>
-      <td>${ath_price}</td>
-      <td>${percent_from_price_ath}</td>`
-    
+  element.className = 'coin-row';
+  
+  const {name, id, symbol, rank, last_updated, quotes:{USD:{price, market_cap, volume_24h, percent_change_15m, percent_change_6h, percent_change_12h, percent_change_24h, percent_change_7d, percent_change_30d, percent_change_1y, ath_price, percent_from_price_ath}}} = props;
+  const updateDate = new Date(last_updated);
+  const updateTime = updateDate.toLocaleTimeString();
 
+
+
+  element.innerHTML = String.raw`
+      <td>${rank}</td>
+      <td><img class="coin-logo" src="https://static.coinpaprika.com/coin/${id}/logo.png?rev=10557311">${symbol}</td>
+      <td>${name}</td>
+      <td>${moneyFormatter.format(price)}</td>
+      <td>${moneyFormatter.format(ath_price)}</td>
+      <td>${moneyFormatter.format(market_cap)}</td>
+      <td>${moneyFormatter.format(volume_24h)}</td>
+      <td class="change">${percent_change_15m}</td>
+      <td class="change">${percent_change_6h}</td>
+      <td class="change">${percent_change_12h}</td>
+      <td class="change">${percent_change_24h}</td>
+      <td class="change">${percent_change_7d}</td>
+      <td class="change">${percent_change_30d}</td>
+      <td>${updateTime}</td>
+      <td><img src="https://graphs.coinpaprika.com/currency/chart/${id}/7d/chart.svg"></td>`
+
+    changeColor('.change')
     return element;
 } 
 
+function changeColor(className) {
+  document.querySelectorAll(className).forEach(elem => {
+    if(Number(elem.innerText) < 0) {
+      elem.classList.add('decreasing');
+    } else if(Number(elem.innerText) > 0) {
+      elem.classList.add('increasing');
+    };
+  });
+}
 
