44e862f19a5fb50f18bfbbdc7ddeac3209ffee89
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-04 16:37:28 +0200

    listener for input field was added

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,50 +1,57 @@
 'use strict';
 
-import { USER_INTERFACE_ID, GLOBAL_INFO_ID, API_BASE_URL, GLOBAL_INFO, ARRAY_OF_COIN_IDS, TICKERS, COINS } from '../constants.js';
-import { createGlobalInfo, createCoinsTable } from '../views/landingView.js';
-import { fetchData } from '../lib/fetchData.js';
-import { moneyFormatter } from '../lib/moneyFormatter.js';
+import {GLOBAL_INFO_ID, API_BASE_URL, GLOBAL_INFO, ARRAY_OF_COIN_IDS, TICKERS, COINS, START_INDEX, END_INDEX, END_INDEX_NAVBAR, INPUT_FIELD, NAVBAR_COINS_ID} from '../constants.js';
+import {createGlobalInfo, createCoinsTable} from '../views/landingView.js';
+import {fetchData} from '../lib/fetchData.js';
+import {moneyFormatter} from '../lib/moneyFormatter.js';
+import {showSearchResults} from './searchPage.js';
 
 export const initLandingPage = async () => {
-  const globalInfo = document.getElementById(GLOBAL_INFO_ID);
   
+  const globalInfo = document.getElementById(GLOBAL_INFO_ID);
+
   const globalResponse = await fetchData(`${API_BASE_URL}${GLOBAL_INFO}`);
   const globalData = createGlobalInfo(globalResponse);
   globalInfo.appendChild(globalData);
 
   const table = document.getElementById('coins');
-  const coinsInfo = await Promise.all(ARRAY_OF_COIN_IDS.map(async coin => {
-    coin = await fetchData(`${API_BASE_URL}${TICKERS}${coin}`);
-    return coin;
-  }));
+  const coinsInfo = await Promise.all(
+    ARRAY_OF_COIN_IDS.slice(START_INDEX, END_INDEX).map(async coin => {
+      coin = await fetchData(`${API_BASE_URL}${TICKERS}${coin}`);
+      return coin;
+    }),
+  );
   
   coinsInfo
-  .sort((a,b) => a.rank - b.rank)
-  .forEach(coin => {
-    const coinRow = createCoinsTable(coin)
-    table.appendChild(coinRow);
+    .sort((a, b) => a.rank - b.rank)
+    .forEach(coin => {
+      const coinRow = createCoinsTable(coin);
+      table.appendChild(coinRow);
 
-    coinRow.onclick = function(e) {
-      openExplorerPage(coin);
-    };
-  });
+      coinRow.onclick = function () {
+        openExplorerPage(coin);
+      };
+    });
 
-  const navbarCoins = coinsInfo.slice(0, 5);
-  const navbarCoinsDiv = document.getElementById('navbar-coins');
+  const navbarCoins = coinsInfo.slice(START_INDEX, END_INDEX_NAVBAR);
+  
+  const navbarCoinsDiv = document.getElementById(NAVBAR_COINS_ID);
   navbarCoins.forEach(coin => {
     const coinDiv = createNavbarCoinDiv(coin);
-    coinDiv.onclick = function() {
+    coinDiv.onclick = function () {
       openExplorerPage(coin);
-    }
+    };
     navbarCoinsDiv.appendChild(coinDiv);
-  })
+  });
 
+  const inputField = document.getElementById(INPUT_FIELD);
+  inputField.addEventListener('keyup', showSearchResults);
 };
 
-async function openExplorerPage(coin) {
+export async function openExplorerPage(coin) {
   const response = await fetchData(`${API_BASE_URL}${COINS}${coin.id}`);
   window.open(response.links.explorer[0]);
-};
+}
 // alternative: goes to static web page with the id of the coin
 
 function createNavbarCoinDiv(coin) {
@@ -57,12 +64,13 @@ function createNavbarCoinDiv(coin) {
   const change = coin.quotes.USD.percent_change_15m;
   coinPrice.textContent = moneyFormatter.format(price);
   coinDiv.append(coinSymbol, coinPrice);
-  if(change < 0) {
+  if (change < 0) {
     coinDiv.classList.add('decreasing');
-  } else if(change > 0) {
+  } else if (change > 0) {
     coinDiv.classList.add('increasing');
-  };
-    return coinDiv;
+  } else {
+    coinDiv.classList.add('unchanged');
+  }
+  return coinDiv;
 }
-
 
