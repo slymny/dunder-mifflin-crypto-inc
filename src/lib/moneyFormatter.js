93fda6f69ed44ea08010702254afe77ed679f6ef
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-04 11:15:30 +0200

    money formatter function added

--- src/lib/moneyFormatter.js
+++ src/lib/moneyFormatter.js
@@ -1,7 +1,7 @@
 export const moneyFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
   });
 
    // default moneyFormatter;
