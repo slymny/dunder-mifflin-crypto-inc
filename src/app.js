e32303630e4f42d5f659f671e30c88d2b00e9982
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-05 23:00:45 +0200

    setInterval is added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@
 import { initLandingPage } from './pages/landingPage.js';
 
 const loadApp = () => {
-  initLandingPage(); 
+  
+  initLandingPage();
+  setInterval(() => {
+    document.getElementById("global-info").innerHTML = '';
+    document.getElementById('coins').innerHTML = '';
+    document.getElementById("navbar-coins").innerHTML = '';
+    
+    initLandingPage();
+  }, 5000); 
 };
 
 window.addEventListener('load', loadApp);
